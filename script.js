6476ead59aa3a6e5ae3131526561352e4bc3941f
Guille <gtiscornia@outlook.com>
2024-06-16 17:11:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 document.getElementById("materialForm").addEventListener("submit", function(event) {
         event.preventDefault();
-        var materialName = document.getElementById("materialName").value;
+        
+    
+    
+    
+    
+    
+        var materialName = document.getElementById("materialName").value;
         var materialThickness = document.getElementById("materialThickness").value;
         var materialTableBody = document.getElementById("materialTableBody");
         var newRow = document.createElement("tr");
         var nameCell = document.createElement("td");
         var thicknessCell = document.createElement("td");
         var actionsCell = document.createElement("td");
-        nameCell.textContent = materialName;
+        
+    
+        fetch('materials.csv')
+        .then(response => response.text())
+        .then(data => {
+        const rows = data.split('\n').slice(1); // Skip the header row
+        const materialNames = document.getElementById('materialNames');
+    
+        rows.forEach(row => {
+            const [name] = row.split(',');
+            const option = document.createElement('option');
+            option.value = name.trim();
+            materialNames.appendChild(option);
+        });
+        })
+        .catch(error => console.error('Error fetching materials.csv:', error));
+    
+    
+    
+    
+        nameCell.textContent = materialName;
         thicknessCell.textContent = materialThickness;
         actionsCell.innerHTML = '<button class="edit-button">Editar</button> <button class="delete-button">Eliminar</button>';
         newRow.appendChild(nameCell);
@@ -17,20 +43,7 @@ document.getElementById("materialForm").addEventListener("submit", function(even
         document.getElementById("materialName").value = "";
         document.getElementById("materialThickness").value = "";
     
-    fetch('materials.csv')
-    .then(response => response.text())
-    .then(data => {
-      const rows = data.split('\n').slice(1); // Skip the header row
-      const materialNames = document.getElementById('materialNames');
-  
-      rows.forEach(row => {
-        const [name] = row.split(',');
-        const option = document.createElement('option');
-        option.value = name.trim();
-        materialNames.appendChild(option);
-      });
-    })
-    .catch(error => console.error('Error fetching materials.csv:', error));
+
 
         // Add event listeners to edit and delete buttons
         var editButtons = document.querySelectorAll(".edit-button");
